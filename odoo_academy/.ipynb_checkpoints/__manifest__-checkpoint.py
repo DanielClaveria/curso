6498419693ac9a62f07be67c272aff4e187b0591
Daniel Claveria <dclaver@ccu.cl>
2021-07-13 02:05:12 +0000

    version 1

---
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    
+    'summary':"""Academy app to manage Training""",
+    
+    'description': """
+        Academy Módule to manage Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Daniel Clavería',
+    
+    'website': 'https://espaciofuturo.cl',
+    
+    'category': 'Training',
+    
+    'version': '0.1',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo':[
+        
+    ],
+}
